fb2aeac61891519c80d54daa7127195035d0feda
AshishUpalekar <ashish.anil.upalekar@accenture.com>
2016-08-10 11:48:24 +0530

    new func.

---
+++ Learning8/Controllers/LuceneController.cs
@@ -0,0 +1,29 @@
+﻿using Lucene.Net.Search;
+using Sitecore.Data.Items;
+using Sitecore.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning8.Controllers
+{
+    public class LuceneController : Controller
+    {
+        // GET: Lucene
+        public void Index()
+        {
+            SearchManager.GetIndex("app_custom_master_index").Rebuild();
+            //SearchManager.GetIndex("sitecore_master_index");
+
+            using (IndexSearchContext indexSearchContext = SearchManager.GetIndex("app_custom_master_index").CreateSearchContext())
+            {
+                // MatchAllDocsQuery will return everything. Use proper query from the link below
+                SearchHits hits = indexSearchContext.Search(new MatchAllDocsQuery(), int.MaxValue);
+                // Get Sitecore items from the results of the query
+                List<Item> items = hits.FetchResults(0, int.MaxValue).Select(result => result.GetObject<Item>()).Where(item => item != null).ToList();
+            }
+        }
+    }
+}
---
+++ Learning8/Controllers/LuceneSearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning8.Controllers
+{
+    public class LuceneSearchController : Controller
+    {
+        // GET: LuceneSearch
+        public void Index()
+        {
+            //var index = ContentSearchManager.GetIndex("sitecore_web_index");
+            //using (var context = index.CreateSearchContext())
+            //{
+            //    var results = context.GetQueryable<SearchResultItem>()
+            //            .Where(item => item.TemplateName == "Sample Item")
+            //            .Where(item => item.Language == "en")
+            //            .Take(10)
+            //            .ToList();
+            //}
+        }
+    }
+}
